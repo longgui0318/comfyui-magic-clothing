b653087552127be0e81c7088a8ab03a6dc32c725
gubei <gubei@duoge.work>
2024-03-24 23:23:14 +0800

    code backup

--- oms_diffusion_nodes.py
+++ oms_diffusion_nodes.py
@@ -30,11 +30,18 @@ class ExtractFeaturesWithAttention:
 
         tokens = clip.tokenize("")
         cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
+        print(id(model))
         model = model.clone()
-        unet = model.model
-        if model.model.in_channels()
-        unet.
-        self.inner_model, x, timestep, uncond, cond, cond_scale, model_options=model_options, seed=seed
+        print(id(model))
+        if model.model.diffusion_model.in_channels ==9:
+            # 如果输入的通道数是9，转成4，这时候，需要注意的就是要重新构建一个出来，不然这个数据会出现错误
+            print(id(model.model))
+            model.model =copy.copy(model.model)
+            print(id(model.model))
+            model.model.diffusion_model = UNetModel()
+            
+        # if model.model.in_channels
+        # self.inner_model, x, timestep, uncond, cond, cond_scale, model_options=model_options, seed=seed
 
         
         return model, feature_image
